1246e34e7bc35ef5d87a9bb58d1d9fc2dc6525cb
Ubuntu <ubuntu@ip-172-31-82-10.ec2.internal>
2020-01-19 14:47:46 +0000

    Initial script added. game.js updated.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+// The script starts when the website is loaded
+$(document).ready(function () {
+
+    // Hide numbers and number button until the game starts
+    $('.first-col').hide();
+    $('.second-col').hide();
+    $(".buttons").hide();
+    console.log('The interactive numbers and buttons are hidden.');
+
+    // Mark selected difficulty level
+    $('.dropdown-item').click(function () {
+        $('.dropdown-item').removeClass('dropdown-item-checked');
+        $(this).addClass('dropdown-item-checked');
+        console.log('The difficulty has changed to ' + $(this).text());
+    });
+    
+});
