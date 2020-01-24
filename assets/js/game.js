fd08003ace62a347516660bb71455d6bcfe63128
Ubuntu <ubuntu@ip-172-31-82-10.ec2.internal>
2020-01-24 22:15:46 +0000

    game.js rewritten. Also restarting game is working now.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,160 +1,146 @@
 // The script starts when the website is loaded
 $(document).ready(function () {
 
-    console.log('DOM is ready. Click on Start to play!');
-
     // Function to mark selected difficulty level
     $('.dropdown-item').click(function () {
         $('.dropdown-item').removeClass('dropdown-item-checked');
         $(this).addClass('dropdown-item-checked');
-
         console.log('The difficulty has changed to ' + $(this).text());
     });
+
+    // Function to determine difficulty level from the .dropdown-item-checked class
+    function getDifficulty(linkText) {
+        switch (linkText) {
+            case 'Easy':
+                return 3;
+            case 'Normal':
+                return 2;
+            case 'Hard':
+                return 1;
+        }
+    }
+
+    // Function to generate a random number string from the round and difficulty variables
+    function getRandomNumbers(round, difficulty) {
+        var lengthOfString = 3 + Math.floor(round / difficulty), // determine the number of numbers using a simple equation
+            randomString = ''; // variable to store random numbers as string
+
+        for (i = 0; i < lengthOfString; i++) { // generating random numbers
+            var num = Math.floor(Math.random() * 10);
+            randomString += num.toString(); // and store them in rndStr variable
+        }
+        console.log(lengthOfString + ' random numbers are generated.');
+        return randomString; // return the value of randomString
+    }
+
+    // Function to update display
+    function updateDisplay(score, lives, timer) {
+        $('.score').text(score);
+        $('.lives').text(lives);
+        $('.timer').text(timer);
+    }
+
+    // Function to wait 2 seconds
+    function wait2Seconds() {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, 2000);
+        });
+    }
+
+    // Function to show numbers to memorize and hide them after
+    async function showHideNumbers(string) {
+        $('.display').text(string).show();
+        var x = await wait2Seconds();
+        console.log('Numbers displayed for 2 seconds.');
+        $('.display').text('.');
+        $('.btn-custom').removeClass('disabled');
+        console.log('Repeat numbers:');
+    }
+
+    // Feedback to console
+    console.log('DOM is ready. Click on Start to play!');
 
     // Function to start the game
     $('#lnkStart').click(function () {
 
-        // Function to determine difficulty level from the .dropdown-item-checked class
-        function getDifficulty() {
-            switch ($('.dropdown-item-checked').text()) {
-                case 'Easy':
-                    return 3;
-                case 'Normal':
-                    return 2;
-                case 'Hard':
-                    return 1;
-            }
-        }
-
-        // Function to generate a random nuber string from the round and difficulty variables
-        function getRandomNumbers(rnd, diff) {
-            var numberOfNumbers = 3 + Math.floor(rnd / diff), // determine the number of numbers using a simple equation
-                rndStr = ''; // variable to store random numbers as string
-
-            for (i = 0; i < numberOfNumbers; i++) { // generating random numbers
-                var num = Math.floor(Math.random() * 10);
-                rndStr += num.toString(); // and store them in rndStr variable
-            }
-            console.log(numberOfNumbers + ' random numbers are generated.');
-            return rndStr; // return the value of rndStr
-        }
-
-        // Function to update display
-        function updateDisplay(score, lives, timer) {
-            $('.score').text(score);
-            $('.lives').text(lives);
-            $('.timer').text(timer);
-        }
-
-        // Function to wait 2 seconds
-        function resolveAfter2Seconds() {
-            return new Promise(resolve => {
-                setTimeout(() => {
-                    resolve();
-                }, 2000);
-            });
-        }
-
-        // Function to show numbers to memorize and hide them after
-        async function showHideNumbers(str) {
-            $('.display').text(str).show();
-            var x = await resolveAfter2Seconds();
-            console.log('Numbers displayed for 2 seconds.');
-
-            $('.display').text('.');
-            $('.btn-custom').removeClass('disabled');
-
-            console.log('Repeat numbers:');
-        }
-
-        async function showWin() {
-            $('.btn-custom').addClass('disabled');
-            $('.display').text('Correct!');
-            console.log('Input numbers correct!');
-            var x = await resolveAfter2Seconds();
-            inputStr = '';
-            round++;
-            score += randomStr.length * 10;
-            console.log('Round ' + (round + 1) + '.');
-            randomStr = getRandomNumbers(round, difficulty);
-            updateDisplay(score, lives, timer);
-            showHideNumbers(randomStr);
-        }
-
-        async function showLose() {
-            $('.btn-custom').addClass('disabled');
-            $('.display').text('INCORRECT!');
-            console.log('Input numbers INCORRECT!');
-            var x = await resolveAfter2Seconds();
-            inputStr = '';
-            lives--;
-            if (lives === 0) {
-                try {
-                    console.log('Game over!');
-                    throw new Error();
-                } catch {
-                    alert('Game over!\nYour final score is ' + score);
-                    location.reload(true);
-                }
-            } else {
-                console.log('Round ' + (round + 1) + '.');
-                randomStr = getRandomNumbers(round, difficulty);
-                updateDisplay(score, lives, timer);
-                showHideNumbers(randomStr);
-            }
-        }
-
         // Function to test random and input strings and compare them
-        function testStrings() {
-            if (randomStr.length === inputStr.length) {
-                if (randomStr === inputStr) {
-                    showWin();
+        async function testStrings() {
+            if (randomString.length === inputString.length) {
+                if (randomString === inputString) {
+                    $('.btn-custom').addClass('disabled');
+                    $('.display').text('Correct!');
+                    console.log('Input numbers correct!');
+                    var x = await wait2Seconds();
+                    inputString = '';
+                    round++;
+                    score += randomString.length * 10;
+                    console.log('Round ' + (round + 1) + '.');
+                    randomString = getRandomNumbers(round, difficulty);
+                    updateDisplay(score, lives, timer);
+                    showHideNumbers(randomString);
                 } else {
-                    showLose();
+                    $('.btn-custom').addClass('disabled');
+                    $('.display').text('INCORRECT!');
+                    console.log('Input numbers INCORRECT!');
+                    var x = await wait2Seconds();
+                    inputString = '';
+                    lives--;
+                    if (lives === 0) {
+                        console.log('Game over!');
+                        alert('Game over!\nYour final score is ' + score);
+                        $('.score').text('');
+                        $('.lives').text('');
+                        $('.timer').text('');
+                        $('#lnkStart').removeClass('disabled');
+                        $('.dropdown-toggle').removeClass('disabled');
+                        $('#lnkAbout').removeClass('disabled');
+                        $('.display').text('.');
+                        $('.btn-custom').off();
+                    } else {
+                        console.log('Round ' + (round + 1) + '.');
+                        randomString = getRandomNumbers(round, difficulty);
+                        updateDisplay(score, lives, timer);
+                        showHideNumbers(randomString);
+                    }
                 }
             }
         }
 
-        // Initialize the game
-        console.log('The game has been started.');
-
+        // Initialize the variables and the game 
         var lives = 3, // variable for the number of lives
             round = 0, // variable for the number of rounds
             score = 0, // variable for the score
             timer = 10, // variable for the timer
-            randomStr = "", // variable for the random numbers to memorize
-            inputStr = "", // variable for entering the memorized numbers
-            //currentTime = 0, // variable to assist the timer
-
-            difficulty = getDifficulty(); // determine difficulty from the .dropdown-item-checked class
+            randomString = "", // variable for the random numbers to memorize
+            inputString = "", // variable for entering the memorized numbers
+            difficulty = getDifficulty($('.dropdown-item-checked').text()); // determine difficulty from the .dropdown-item-checked class
 
         // Disable menu after start (except home link)
         $('#lnkStart').addClass('disabled');
         $('.dropdown-toggle').addClass('disabled');
         $('#lnkAbout').addClass('disabled');
-
         console.log('The game has been initialized.');
 
-        // Get the random numbers, show for 2 seconds and hide again
-        randomStr = getRandomNumbers(round, difficulty);
+        // Get the random numbers, show them for 2 seconds and hide them again
+        randomString = getRandomNumbers(round, difficulty);
         console.log('Round ' + (round + 1) + '.');
         updateDisplay(score, lives, timer);
-
-        showHideNumbers(randomStr);
+        showHideNumbers(randomString);
 
         // Functions to click number buttons
         $('.btn-custom').click(function () {
+            // No input in case if buttons are disabled
             if ($(this).hasClass('disabled')) {
                 console.log('Button ' + $(this).text() + ' disabled!');
                 return false;
             }
 
-            inputStr += $(this).text();
-            $('.display').text(inputStr);
+            inputString += $(this).text();
+            $('.display').text(inputString);
             console.log('Number ' + $(this).text() + ' pressed.');
             testStrings();
         });
-
     });
-
 });
